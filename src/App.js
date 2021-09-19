278a46a4052afeb543daaeb75123d86b52c72adf
nour-gamal <nmg181963@hotmail.com>
2021-09-19 17:56:05 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Routes from "./Routes";
+import "antd/dist/antd.css";
 
 function App() {
-	return <div className="App">cleanup 3</div>;
+	return (
+		<div className="App">
+			<Routes />
+		</div>
+	);
 }
 
 export default App;
