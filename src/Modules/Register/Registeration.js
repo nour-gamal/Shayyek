5a4ee7707356e41c71df0566320f77d3e424a29a
Nancy Ahmed <anancy712@gmail.com>
2021-09-21 14:44:54 +0200

    create components in Registeration Folder

---
+++ src/Modules/Register/Registeration.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RegisterationForm from './Components/RegisterationForm/RegisterationForm'
+
+function Registeration() {
+    return (
+        <div>
+            <RegisterationForm />
+        </div>
+    )
+}
+
+export default Registeration
