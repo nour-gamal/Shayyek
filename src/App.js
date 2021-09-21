f4b7368ed33d31b984bf6efa4e6e72a125660ef9
Nancy Ahmed <anancy712@gmail.com>
2021-09-21 10:26:56 +0200

    Registration form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import Routes from "./Routes";
 import "antd/dist/antd.css";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
