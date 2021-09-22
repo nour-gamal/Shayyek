f72c5205d803fe5a4a281e9a35d453e2aefe008d
nour-gamal <nmg181963@hotmail.com>
2021-09-22 14:20:10 +0200

    clean up

--- src/Modules/Common/Navbar/Navbar.js
+++ src/Modules/Common/Navbar/Navbar.js
@@ -5,13 +5,17 @@ import languages from "../../../Resources/Assets/languages.svg";
 import { changeLocal } from "../../../Redux/Localization";
 import shayyek_logo_dark from "../../../Resources/Assets/shayyek_logo_dark.svg";
 import shayyek_logo_light from "../../../Resources/Assets/shayyek_logo_light.svg";
+import AllSuppliers from "../../../Resources/Assets/All_suppliers.svg";
+import cart from "../../../Resources/Assets/cart.svg";
+import Request_RFQ from "../../../Resources/Assets/Request_RFQ.svg";
+
 import "./Navbar.css";
 function Navbarr({ navState }) {
 	const { currentLocal } = useSelector((state) => state.currentLocal);
 	const dispatch = useDispatch();
 
 	return (
-		<Navbar expand="lg" className={navState ? "light" : "dark"}>
+		<Navbar expand="md" className={navState ? "light" : "dark"}>
 			<Container>
 				<Navbar.Brand href="/">
 					<img
@@ -24,15 +28,28 @@ function Navbarr({ navState }) {
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav>
 							<div className="centerNav">
-								<Nav.Link href="#home">Home</Nav.Link>
-								<Nav.Link href="#link">Link</Nav.Link>
+								<Nav.Link href="#home">
+									<img src={AllSuppliers} alt="AllSuppliers" />
+									{currentLocal.navbar.AllSupplies}
+								</Nav.Link>
+								<Nav.Link href="#link">
+									<img src={Request_RFQ} alt="Request_RFQ" />
+									{currentLocal.navbar.requestRFQ}
+								</Nav.Link>
+								<Nav.Link href="#link">
+									<img src={cart} alt="cart" />
+									{currentLocal.navbar.cart}
+								</Nav.Link>
 							</div>
 							<div className="endNav">
+								<Nav.Link href="#">{currentLocal.navbar.signin}</Nav.Link>
+								<Nav.Link href="#">{currentLocal.navbar.register}</Nav.Link>
+
 								<Nav.Link href="#">
 									<img
 										src={languages}
 										alt="languages"
-										className={"langIcon"}
+										className="langIcon"
 										onClick={() => {
 											dispatch(
 												changeLocal(
