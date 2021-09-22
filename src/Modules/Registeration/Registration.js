cb1dc7712cdb5f14b82cf64b2366f18ae75576b8
Nancy Ahmed <anancy712@gmail.com>
2021-09-22 13:06:05 +0200

    add navbar

--- src/Modules/Registeration/Registration.js
+++ src/Modules/Registeration/Registration.js
@@ -1,14 +1,16 @@
 import React from "react";
 import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
 import Footer from "../Common/Footer/Footer";
+import Navbar from "../Common/Navbar/Navbar";
 
 function Registeration() {
-	return (
-		<div>
-			<RegistrationForm />
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Navbar navState={"light"} />
+      <RegistrationForm />
+      <Footer />
+    </div>
+  );
 }
 
 export default Registeration;
