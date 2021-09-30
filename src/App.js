4b620d16e4b73ccd7d23b31ad80935b14f03d483
nour-gamal <nmg181963@hotmail.com>
2021-09-30 13:04:36 +0200

    get device token

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
-import "./App.css";
+import { useEffect } from "react";
 import Routes from "./Routes";
 import { useSelector } from "react-redux";
+import { getMessaging, getToken } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
 import "antd/dist/antd.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import '@progress/kendo-theme-default/dist/all.css';
+import "./App.css";
 
 function App() {
 	const { currentLocal } = useSelector((state) => state.currentLocal);
 
+	const firebaseConfig = {
+		apiKey: "AIzaSyDl8exN4m1F52c8fe9dGlMKKhuWotvzF9U",
+		authDomain: "shayyek-cc93c.firebaseapp.com",
+		projectId: "shayyek-cc93c",
+		storageBucket: "shayyek-cc93c.appspot.com",
+		messagingSenderId: "22834068986",
+		appId: "1:22834068986:web:d85ffc0deb26fd755bf843",
+		measurementId: "G-JCL13JT214",
+	};
+	initializeApp(firebaseConfig);
+
+	useEffect(() => {
+		const messaging = getMessaging();
+		getToken(messaging, {
+			vapidKey:
+				"BHH-6zNNTm-rC6X7rfazhJlDc4HnCYhoEjN-A4qCuoDwiNUkoORYqd25nBMPm4WMMdfW7GTlk6o5eaRROAbn_NQ",
+		})
+			.then((currentToken) => {
+				if (currentToken) {
+					// Send the token to your server and update the UI if necessary
+					// ...
+					console.log("currentToken:", currentToken);
+				} else {
+					// Show permission request UI
+					console.log(
+						"No registration token available. Request permission to generate one."
+					);
+					// ...
+				}
+			})
+			.catch((err) => {
+				console.log("An error occurred while retrieving token. ", err);
+				// ...
+			});
+	}, []);
+
 	document
 		.querySelector("html")
 		.setAttribute("dir", currentLocal.language === "English" ? "ltr" : "rtl");
+
 	return (
 		<div className="App">
 			<Routes />
