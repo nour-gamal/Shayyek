924f1d175d378391bf389d49518f68818ee48efd
Nancy Ahmed <anancy712@gmail.com>
2021-09-30 12:06:27 +0200

    handle padding

--- src/Modules/Common/AuthHeader/AuthHeader.js
+++ src/Modules/Common/AuthHeader/AuthHeader.js
@@ -5,78 +5,75 @@ import { Col, Row } from "antd";
 import { useSelector, useDispatch } from "react-redux";
 import { Dropdown } from "react-bootstrap";
 import languages from "../../../Resources/Assets/languages.svg";
-function AuthHeader({title , showState}) {
+function AuthHeader({ title, showState }) {
   const { currentLocal } = useSelector((state) => state.currentLocal);
   const dispatch = useDispatch();
   const [buyer, setBuyer] = useState("");
 
-	const toggleKind = (e) => {
-		e.preventDefault();
-		if (e.target.id === "buyer") {
-			setBuyer(e.target.id);
-		} else if (e.target.id === "Contractor") {
-			setBuyer(e.target.id);
-		
-		} else if (e.target.id === "supplies") {
-			setBuyer(e.target.id);
-	
-		}
-	};
+  const toggleKind = (e) => {
+    e.preventDefault();
+    if (e.target.id === "buyer") {
+      setBuyer(e.target.id);
+    } else if (e.target.id === "Contractor") {
+      setBuyer(e.target.id);
+    } else if (e.target.id === "supplies") {
+      setBuyer(e.target.id);
+    }
+  };
   return (
     <div className="AuthHeader">
       <Row>
-					<Col md={12} xs={24}>
-			      <div className="f-27 mx-3 title">{title}</div>
+        <Col md={12} xs={12}>
+          <div className="f-27 mx-3 title">{title}</div>
+        </Col>
 
-					</Col>
-        
-					<Col md={12} xs={24}>
-  
-						<div className="dropdownmenu">
-            {showState &&
-							<Dropdown>
-								<Dropdown.Toggle id="dropdown-basic">{buyer?buyer:currentLocal.registration.userType}</Dropdown.Toggle>
+        <Col md={12} xs={12}>
+          <div className="dropdownmenu">
+            {showState && (
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic">
+                  {buyer ? buyer : currentLocal.registration.userType}
+                </Dropdown.Toggle>
 
-								<Dropdown.Menu>
-									<Dropdown.Item value="buyer" id="buyer" 
-                  onClick={toggleKind}
+                <Dropdown.Menu>
+                  <Dropdown.Item value="buyer" id="buyer" onClick={toggleKind}>
+                    {currentLocal.registration.buyer}
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    value="Contractor"
+                    id="Contractor"
+                    onClick={toggleKind}
                   >
-										{currentLocal.registration.buyer}
-									</Dropdown.Item>
-									<Dropdown.Item
-										value="Contractor"
-										id="Contractor"
-										onClick={toggleKind}
-									>
-										{currentLocal.registration.Contractor}
-									</Dropdown.Item>
-									<Dropdown.Item
-										value="supplies"
-										id="supplies"
-										onClick={toggleKind}
-									>
-										{currentLocal.registration.supplies}
-									</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown>
-            }
-							<img
-								src={languages}
-								alt="languages"
-								onClick={() => {
-									dispatch(
-										changeLocal(
-											currentLocal.language === "English" ? "ar" : "en"
-										)
-									);
-								}}
-								className="languages"
-							/>
-						</div>
-					</Col>
-          
-				</Row>
-
+                    {currentLocal.registration.Contractor}
+                  </Dropdown.Item>
+                  <Dropdown.Item
+                    value="supplies"
+                    id="supplies"
+                    onClick={toggleKind}
+                  >
+                    {currentLocal.registration.supplies}
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            )}
+		
+			<span> 
+			{currentLocal.language==="العربيه" ? "ar" :"en"} 
+            <img
+              src={languages}
+              alt="languages"
+              onClick={() => {
+                dispatch(
+                  changeLocal(currentLocal.language === "English" ? "ar" : "en")
+                );
+              }}
+              className="languages"
+            />
+			</span>
+		
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
