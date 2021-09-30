2ebe7918f9cdc7d6c921209aad101b916f2fb1c2
nour-gamal <nmg181963@hotmail.com>
2021-09-30 13:40:21 +0200

    changing logos to components

--- src/Modules/Common/Footer/Footer.js
+++ src/Modules/Common/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import shayyek_logo_light from "../../../Resources/Assets/shayyek_logo_light.svg";
+import ShayyekLogoLight from "../../../Resources/Assets/shayyek_logo_light";
 import "./Footer.css";
 
 function Footer() {
@@ -9,7 +9,7 @@ function Footer() {
 	return (
 		<footer className="footer">
 			<div>
-				<img src={shayyek_logo_light} alt="shayyek_logo_light" />
+				<ShayyekLogoLight />
 			</div>
 			<div className="divider"></div>
 			<div className="f-14">&copy; {currentLocal.footer.copyRights}</div>
