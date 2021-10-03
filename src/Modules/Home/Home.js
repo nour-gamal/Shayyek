78cde3382a5fc57b75b91ec8e72697de5c8faa7a
nour-gamal <nmg181963@hotmail.com>
2021-10-03 16:54:50 +0200

    clean up

--- src/Modules/Home/Home.js
+++ src/Modules/Home/Home.js
@@ -1,50 +1,7 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { login } from "../../Redux/Authorization";
+import React from "react";
 import BuyerHome from "../BuyerHome/BuyerHome";
 import Navbar from "../Common/Navbar/Navbar";
-import { loginFun, getAllUsers, userProfile } from "./network";
 function Home() {
-	const dispatch = useDispatch();
-
-	useEffect(() => {
-		const body = {
-			email: "nmg18196@hotmail.com",
-			password: "Asd@123",
-		};
-
-		loginFun(
-			body,
-			(res) => {
-				localStorage.setItem("token", res.token);
-				dispatch(login(res.user));
-			},
-			(e) => {
-				console.log(e);
-			},
-			false
-		);
-
-		getAllUsers(
-			(res) => {
-				//console.log(res);
-			},
-			(e) => {
-				//console.log(e);
-			},
-			false
-		);
-
-		userProfile(
-			(res) => {
-				//console.log(res);
-			},
-			(e) => {
-				//console.log(e);
-			}
-		);
-	}, [dispatch]);
-
 	return (
 		<div>
 			<Navbar />
