d5062124e368e99dfa94e3e02763e6c2466cd178
nour-gamal <nmg181963@hotmail.com>
2021-10-06 17:52:13 +0200

    finish import from excel sheet

--- src/Modules/Home/BuyerHome/Components/CreateRFQ/CreateRFQ.js
+++ src/Modules/Home/BuyerHome/Components/CreateRFQ/CreateRFQ.js
@@ -3,6 +3,7 @@ import { Table } from "antd";
 import importIcon from "../../../../../Resources/Assets/import.svg";
 import addIcon from "../../../../../Resources/Assets/addIcon.svg";
 import { useSelector } from "react-redux";
+import { ExcelRenderer } from "react-excel-renderer";
 import "./CreateRFQ.css";
 function CreateRFQ() {
 	const { currentLocal } = useSelector((state) => state.currentLocal);
@@ -17,6 +18,33 @@ function CreateRFQ() {
 				key,
 			},
 		]);
+	};
+
+	const fileHandler = (event) => {
+		let fileObj = event.target.files[0];
+
+		//just pass the fileObj as parameter
+		ExcelRenderer(fileObj, (err, resp) => {
+			if (err) {
+				console.log(err);
+			} else {
+				resp.rows[0].forEach((item) => {
+					columns.forEach((col) => {
+						if (item === col.title) {
+							resp.rows.forEach((name) => {
+								updateDataSource((oldDataSource, i) => [
+									...oldDataSource,
+									{
+										key: i,
+										item: name[0],
+									},
+								]);
+							});
+						}
+					});
+				});
+			}
+		});
 	};
 
 	const columns = [
@@ -62,7 +90,16 @@ function CreateRFQ() {
 			<div className="actionsContainer">
 				<div>
 					<div className="mb-3">
-						<img src={importIcon} alt="importIcon" className="mx-3" />
+						<input
+							type="file"
+							id="actual-btn"
+							onChange={fileHandler}
+							className="d-none"
+						/>
+
+						<label htmlFor="actual-btn">
+							<img src={importIcon} alt="importIcon" className="mx-3" />
+						</label>
 						<label>{currentLocal.buyerHome.importExcelFile}</label>
 					</div>
 					<div className="mb-3">
