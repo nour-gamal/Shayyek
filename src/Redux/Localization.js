6108c2c5977cff02707474b1120061c123653045
nour-gamal <nmg181963@hotmail.com>
2021-11-07 16:32:34 +0200

    clean up

--- src/Redux/Localization.js
+++ src/Redux/Localization.js
@@ -2,21 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 import { en } from "../Resources/English";
 import { ar } from "../Resources/Arabic";
 export const localizationSlice = createSlice({
-  name: "localization",
-  initialState: {
-    currentLocal: en,
-    currentLanguageId: localStorage.getItem("englishId"),
-  },
-  reducers: {
-    changeLocal: (state, action) => {
-      
-      state.currentLocal = action.payload === "en" ? en : ar;
-      state.currentLanguageId =
-        action.payload === "en"
-          ? localStorage.getItem("englishId")
-          : localStorage.getItem("arabicId");
-    },
-  },
+	name: "localization",
+	initialState: {
+		currentLocal: en,
+		currentLanguageId: localStorage.getItem("englishId"),
+	},
+	reducers: {
+		changeLocal: (state, action) => {
+			state.currentLocal = action.payload === "en" ? en : ar;
+			state.currentLanguageId =
+				action.payload === "en"
+					? localStorage.getItem("englishId")
+					: localStorage.getItem("arabicId");
+		},
+	},
 });
 export const { changeLocal } = localizationSlice.actions;
 
