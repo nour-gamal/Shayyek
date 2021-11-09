7dc672b09f5c37deceaba2d4ea18c165ae80ed9f
nour-gamal <nmg181963@hotmail.com>
2021-11-09 17:49:22 +0200

    clean up

--- src/Modules/Home/BuyerHome/Components/PostRFQModal/PostRFQModal.js
+++ src/Modules/Home/BuyerHome/Components/PostRFQModal/PostRFQModal.js
@@ -1,34 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Modal } from "antd";
+import SelectSearch from "react-select-search";
 import { useSelector } from "react-redux";
-import { Menu } from "antd";
+import PlusCircle from "../../../../../Resources/Assets/plusCircle.svg";
+import WhiteCross from "../../../../../Resources/Assets/whiteCross.svg";
 import "./PostRFQModal.css";
 
 function PostRFQModal({ isModalVisible, onCancel }) {
 	const { currentLocal } = useSelector((state) => state.currentLocal);
+	const [searchValue, updateSearchValue] = useState("");
+
+	const options = [
+		{ name: "Swedish", value: "sv" },
+		{ name: "English", value: "en" },
+		{ name: "French", value: "fr" },
+	];
 
 	return (
 		<Modal
 			title="Basic Modal"
 			visible={isModalVisible}
 			onCancel={onCancel}
-			className="modal-lg postRFQModal"
+			className="modal-lg postRFQModal text-center"
 		>
-			<label>{currentLocal.buyerHome.invitedByEmail}</label>
-			<input
-				list="search"
-				id="searchValue"
-				name="searchValue"
-				placeholder={currentLocal.buyerHome.selectSupplierEmail}
-				className="form-control mx-3 d-inline"
-				// onChange={this.handleSearchOnChange}
-				// value={this.state.searchValue}
-			/>
-			<datalist id="search">
-				<option key={1} value={"hello"} />
-				<option key={3} value={"myy"} />
-				<option key={1} value={"name"} />
-			</datalist>
+			<div className="d-flex align-items-center">
+				<label>{currentLocal.buyerHome.invitedByEmail}</label>
+				<SelectSearch
+					options={options}
+					value={searchValue}
+					onChange={(optionId, selectedOption) => {
+						updateSearchValue(optionId);
+					}}
+					// filterOptions={(e) => {
+					// 	console.log(e);
+					// }}
+					closeOnSelect={true}
+					autoComplete={true}
+					name="language"
+					search={true}
+					placeholder={currentLocal.buyerHome.selectSupplierEmail}
+				/>
+				<img src={PlusCircle} alt="PlusCircle" className="mx-2" />
+				<label>{currentLocal.buyerHome.addNewEmail}</label>
+			</div>
+
+			<div className="capsulesContainer my-4">
+				<span className="orangeCapsule">
+					<span className="mx-2">hello</span>
+					<img src={WhiteCross} alt="WhiteCross" />
+				</span>
+			</div>
 		</Modal>
 	);
 }
