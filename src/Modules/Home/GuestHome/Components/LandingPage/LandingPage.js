fceea94db04ed6dd815da9e71334fbf1cd147b77
Nancy Ahmed <anancy712@gmail.com>
2021-11-10 15:00:49 +0200

    cleanUp

--- src/Modules/Home/GuestHome/Components/LandingPage/LandingPage.js
+++ src/Modules/Home/GuestHome/Components/LandingPage/LandingPage.js
@@ -2,8 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Navbar from "../../../../Common/Navbar/Navbar";
 import SearchShayyek from "../../../../../Resources/Assets/Search Shayyek.svg";
+import Fuse from "fuse.js";
 import "./LandingPage.css";
+import SelectSearch from "react-select-search";
 function LandingPage() {
+  function fuzzySearch(options) {
+		const fuse = new Fuse(options, {
+			keys: ["name", "groupName", "items.name"],
+			threshold: 0.3,
+		});
+
+		return (value) => {
+			if (!value.length) {
+				return options;
+			}
+
+			return fuse.search(value);
+		};
+	}
+    const countries = [
+      {name: 'Swedish', value: 'sv'},
+      {name: 'English', value: 'en'},
+  ];
   const [searchWord, setSearchWord] = useState("");
   useEffect(() => {
     localStorage.setItem("landingPage", LandingPage);
@@ -14,46 +34,53 @@ function LandingPage() {
     console.log(searchWord);
   };
   return (
-    <div className="landingPage">
-      <Navbar transparent={true} />
-      <Container>
-        <Row>
-          <Col md={6} xs={12}>
-            <h2 className="text-white my-0 f-27">Shayyek</h2>
-            <p className="text-white f-21 mb-4 fw-light">
-              {" "}
-              Optimizing Procurement.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6} xs={12}>
-            <h3 className="f-27 my-0">Verified, Connected, Streamlined</h3>
-            <p className="text-white f-21 mb-5 fw-light">
-              Procurement Management.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6} xs={12}>
-            <div className="search">
-              <input
-                type="search"
-                className="input-field form-control px-5 py-4"
-                placeholder="Search by Field, Company name"
-                onChange={handleSearch}
-              />
-              <img
-                src={SearchShayyek}
-                alt="SearchShayyek"
-                className="SearchShayyek"
-              />
-            </div>
-          </Col>
-        </Row>
-        <Row className="liveBoxContainer">
-          <Col md={6} xs={12}>
-            {/* <div className="liveBox">
+    <>
+      <div className="overlay"></div>
+      <div className="landingPage">
+        <Navbar transparent={true} />
+        <Container>
+          <Row>
+            <Col md={6} xs={12}>
+              <h2 className="text-white my-0 f-27">Shayyek</h2>
+              <p className="text-white f-21 mb-4 fw-light">
+                Optimizing Procurement.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} xs={12}>
+              <h3 className="f-27 my-0">Verified, Connected, Streamlined</h3>
+              <p className="text-white f-21 mb-5 fw-light">
+                Procurement Management.
+              </p>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6} xs={12}>
+              <div className="search">
+                <input
+                  type="search"
+                  className="input-field form-control px-5 py-4"
+                  placeholder="Search by Field, Company name"
+                  onChange={handleSearch}
+                />
+                <SelectSearch
+                  options={countries}
+                  search={true}
+                  filterOptions={fuzzySearch}
+                  placeholder="Select your country"
+                />
+                <img
+                  src={SearchShayyek}
+                  alt="SearchShayyek"
+                  className="SearchShayyek"
+                />
+              </div>
+            </Col>
+          </Row>
+          <Row className="liveBoxContainer">
+            <Col md={6} xs={12}>
+              {/* <div className="liveBox">
               <div className="button">
                 <div className="button-primary f-21  fw-bold"> Live</div>
               </div>
@@ -64,25 +91,28 @@ function LandingPage() {
                 </p>
               </div>
             </div> */}
-            <Row>
-              <Col md={6} xs={12}>
-                <div className="button">
-                  <div className="button-primary f-21  fw-bold"> Live</div>
-                </div>
-              </Col>
-              <Col md={6} xs={12}>
-                <div className="lastUpdatStock">
-                  <p className="text-white f-14">
-                    Stock Availability Last Updated
-                    <span className="mx-4">{"Today"}</span>
-                  </p>
-                </div>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+              <Row>
+                <Col md={6} xs={12}>
+                  <div className="button">
+                    <button className="button-primary f-21  fw-bold">
+                      {" "}
+                      Live
+                    </button>
+                  </div>
+                </Col>
+                <Col md={6} xs={12}>
+                  <div className="lastUpdatStock">
+                    <p className="text-white f-14">
+                      Stock Availability Last Updated {"Today"}
+                    </p>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
