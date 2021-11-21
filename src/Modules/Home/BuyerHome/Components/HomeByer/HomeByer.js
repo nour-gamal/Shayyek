f794f791a69137691e2be9638eea8d11507daf15
Nancy Ahmed <anancy712@gmail.com>
2021-11-21 15:30:49 +0200

    home

--- src/Modules/Home/BuyerHome/Components/HomeByer/HomeByer.js
+++ src/Modules/Home/BuyerHome/Components/HomeByer/HomeByer.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import research from "../../../../../Resources/Assets/research@2x.png"
 import "./HomeByer.css";
 function HomeByer() {
   return (
     <div className="homeByer">
       <div className="CompanyContainer">
-        <button className="btn btn-danger mt-4">
+        <Container>
+        <button className="btn btn-danger mt-4 mx-4">
             {/* <img src={Request_RFQ} alt="Request_RFQ" /> */}
             Add New RFQ</button>
+            </Container>
       </div>
-      <div className="projectContainer">projectContainer</div>
+      <div className="projectContainer">
+<div className="invitations px-3">
+<div className="RFQinvitations">
+<img src={research} alt="research" />
+<p className="ml-2">RFQ Invitations</p>
+</div>
+<div className="mt-4">
+  <p>5</p>
+</div>
+</div>
+      </div>
     </div>
   );
 }
