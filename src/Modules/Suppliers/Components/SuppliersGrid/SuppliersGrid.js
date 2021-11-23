0dd0c3d66a4cc247edd6da6ec3e423de963ef805
nour-gamal <nmg181963@hotmail.com>
2021-11-23 16:32:51 +0200

    geall

--- src/Modules/Suppliers/Components/SuppliersGrid/SuppliersGrid.js
+++ src/Modules/Suppliers/Components/SuppliersGrid/SuppliersGrid.js
@@ -65,8 +65,8 @@ function CompaniesGrid() {
 	}, []);
 
 	return (
-		<div className="suppliersGrid">
-			<Container fluid className="pl-0 pr-0">
+		<div className="suppliersGrid ppl ppr">
+			<Container fluid>
 				<Row>
 					{Suppliers.map((supplier, i) => {
 						return (
--- src/Modules/Suppliers/Suppliers.js
+++ src/Modules/Suppliers/Suppliers.js
@@ -1,16 +1,28 @@
 import React from "react";
 import SuppliersGrid from "./Components/SuppliersGrid/SuppliersGrid";
 import NavBar from "../Common/Navbar/Navbar";
-//import { useSelector } from "react-redux";
-//import Filter from "./Components/Filter/Filter";
+import { useSelector } from "react-redux";
+import { Container, Col, Row } from "react-bootstrap";
+import Filter from "./Components/Filter/Filter";
 import Footer from "../Common/Footer/Footer";
 function Companies() {
-	//const { currentLocal } = useSelector((state) => state.currentLocal);
+	const { currentLocal } = useSelector((state) => state.currentLocal);
 	return (
 		<section>
 			<NavBar />
-			<SuppliersGrid />
-			{/* <Filter /> */}
+			<Container
+				fluid
+				className={currentLocal.language === "English" ? "ppl" : "ppr"}
+			>
+				<Row>
+					<Col xs={8} sm={9} lg={10}>
+						<SuppliersGrid />
+					</Col>
+					<Col xs={4} sm={3} lg={2} className="pr-0 pl-0 ">
+						<Filter />
+					</Col>
+				</Row>
+			</Container>
 			<Footer />
 		</section>
 	);
