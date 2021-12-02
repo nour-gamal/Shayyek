409ef8d7d3ab44f4e0dccfe7a6f579b4541ac369
nour-gamal <nmg181963@hotmail.com>
2021-12-02 11:40:43 +0200

    adding excel sheet rendering

--- src/Modules/Home/SupplierHome/Components/AddProductModal/AddProductModal.js
+++ src/Modules/Home/SupplierHome/Components/AddProductModal/AddProductModal.js
@@ -1,13 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import { Modal } from "antd";
 import { useSelector } from "react-redux";
 import closeIcon from "../../../../../Resources/Assets/closeIcon.svg";
 import plusIcon from "../../../../../Resources/Assets/add (3).svg";
 import importIcon from "../../../../../Resources/Assets/import.svg";
+import { OutTable, ExcelRenderer } from "react-excel-renderer";
 import "./AddProductModal.css";
 function AddProductModal({ isModalVisible, onCancel }) {
 	const { currentLocal } = useSelector((state) => state.currentLocal);
+	const [rows, updateRows] = useState([]);
+	const [cols, updateCols] = useState([]);
 
+	const fileHandler = (event) => {
+		let fileObj = event.target.files[0];
+
+		//just pass the fileObj as parameter
+		ExcelRenderer(fileObj, (err, resp) => {
+			if (err) {
+				console.log(err);
+			} else {
+				updateRows(resp.rows);
+				updateCols(resp.cols);
+			}
+		});
+	};
 	return (
 		<Modal
 			title="Basic Modal"
@@ -23,26 +39,33 @@ function AddProductModal({ isModalVisible, onCancel }) {
 			/>
 			<div className="d-flex mx-5  toolsSection">
 				<div>
-					<img src={plusIcon} alt="Plus" className="mx-3" />
+					<img src={plusIcon} alt="Plus" className="mx-2" />
 					<span>{currentLocal.supplierHome.addPriceList}</span>
 				</div>
-				<div className="d-flex align-items-end ">
+				<div className="d-flex align-items-end">
 					<input
 						type="file"
 						id="actual-btn"
-						//onChange={fileHandler}
+						onChange={fileHandler}
 						className="d-none"
 					/>
 
 					<label htmlFor="actual-btn" className="primary-color cursorPointer">
-						<img src={importIcon} alt="importIcon" className="mx-3" />
+						<img src={importIcon} alt="importIcon" className="mx-2" />
 					</label>
 					<label htmlFor="actual-btn" className="cursorPointer">
 						{currentLocal.buyerHome.importExcelFile}
 					</label>
 				</div>
 			</div>
-			<div className="uploadingArea"></div>
+			<div className="uploadingArea">
+				<OutTable
+					data={rows}
+					columns={cols}
+					tableClassName="ExcelTable2007"
+					tableHeaderRowClass="heading"
+				/>
+			</div>
 			<div className="d-flex justify-content-center  mt-4 actionsContainer">
 				<button className="button-secondary mx-2">
 					{currentLocal.supplierHome.cancel}
