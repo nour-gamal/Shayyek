6750a8a9170cb7ff34b775935964198bb159defc
nour-gamal <nmg181963@hotmail.com>
2021-12-14 15:37:45 +0200

    finish rfqDetails

--- src/Modules/Common/ProductDetails/ProductDetails.js
+++ src/Modules/Common/ProductDetails/ProductDetails.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Row, Col } from "antd";
 import { baseUrl } from "../../../Services";
 import { Modal } from "antd";
+import defaultCompImg from "../../../Resources/Assets/defaultCompImg.png";
 import positive from "../../../Resources/Assets/postive.svg";
 import negative from "../../../Resources/Assets/negative.svg";
 import { useSelector } from "react-redux";
@@ -24,63 +25,66 @@ function ProductDetails({ isModalVisible, onCancel, product, parent }) {
 						className="productImg"
 					/>
 				</Col>
-				<Col xs={6} sm={18}>
+				<Col
+					xs={24}
+					sm={18}
+					className="d-flex justify-content-between flex-column"
+				>
 					<div>
 						<div className="productName my-2">{product.name}</div>
-						<div className="d-flex justify-content-between">
+						<div className="d-flex justify-content-between companyInfoContainer">
 							<div className="d-flex align-items-center my-2">
-								{product.companyLogo ? (
+								<div>
 									<img
-										src={baseUrl + product.companyLogo}
+										src={
+											product.companyLogo
+												? baseUrl + product.companyLogo
+												: defaultCompImg
+										}
 										alt="companyLogo"
 										className="companyLogo"
 									/>
-								) : (
-									<img
-										src={"https://place-hold.it/300x300"}
-										alt="companyLogo"
-										className="companyLogo"
-									/>
-								)}
-								<div className="mx-2">{product.companyName}</div>
+								</div>
+								<div className="mx-2 companyName">{product.companyName}</div>
 							</div>
 							<div className="d-flex align-items-center">
-								<div className="priceLabel mx-2">
-									{currentLocal.supplierHome.price}:
-								</div>
-								<div className="price">{product.price} LE</div>
+								<label>{currentLocal.supplierHome.price}:</label>
+								<div className="price mx-2">{product.price} LE</div>
 							</div>
 						</div>
 						<div className="specs my-2">{product.specs}</div>
-						<div className="my-2">
-							<label>
-								{parent === "supplierHome"
-									? currentLocal.supplierHome.sizes
-									: currentLocal.supplierHome.chooseSize}
-							</label>
-							<div className="capsulesContainer">
-								{product.sizes.map((size, index) => (
-									<div className="capsules" key={index}>
-										{size.name}
-									</div>
-								))}
+						{product.sizes.length > 0 && (
+							<div className="my-3">
+								<label>
+									{parent === "supplierHome"
+										? currentLocal.supplierHome.sizes
+										: currentLocal.supplierHome.chooseSize}
+								</label>
+								<div className="capsulesContainer">
+									{product.sizes.map((size, index) => (
+										<div className="capsules f-12" key={index}>
+											{size.name}
+										</div>
+									))}
+								</div>
 							</div>
-						</div>
-						<div className="my-2">
-							<label>
-								{parent === "supplierHome"
-									? currentLocal.supplierHome.models
-									: currentLocal.supplierHome.chooseModel}
-							</label>
-							<div className="capsulesContainer">
-								{product.models.map((model, index) => (
-									<div className="capsules" key={index}>
-										{model.name}
-									</div>
-								))}
+						)}
+						{product.models.length > 0 && (
+							<div className="my-3">
+								<label>
+									{parent === "supplierHome"
+										? currentLocal.supplierHome.models
+										: currentLocal.supplierHome.chooseModel}
+								</label>
+								<div className="capsulesContainer">
+									{product.models.map((model, index) => (
+										<div className="capsules f-12" key={index}>
+											{model.name}
+										</div>
+									))}
+								</div>
 							</div>
-						</div>
-
+						)}
 						<label>{currentLocal.supplierHome.quantity}</label>
 						<div className="d-flex px-2 justify-content-between inputField avQty align-items-center">
 							<div className="d-flex align-items-center">
@@ -98,7 +102,13 @@ function ProductDetails({ isModalVisible, onCancel, product, parent }) {
 										/>
 									</div>
 								)}
-								<div className="quantityCount mx-2">{quantityCount}</div>
+								<div
+									className={
+										parent === "supplierHome" ? "mx-2" : "quantityCount mx-2"
+									}
+								>
+									{quantityCount}
+								</div>
 								{parent !== "supplierHome" && (
 									<div>
 										<img
@@ -114,11 +124,13 @@ function ProductDetails({ isModalVisible, onCancel, product, parent }) {
 							</div>
 						</div>
 					</div>
-					<button className="button-primary">
-						{parent === "supplierHome"
-							? currentLocal.supplierHome.edit
-							: currentLocal.supplierHome.addToCart}
-					</button>
+					<div className="button-container">
+						<button className="button-primary">
+							{parent === "supplierHome"
+								? currentLocal.supplierHome.edit
+								: currentLocal.supplierHome.addToCart}
+						</button>
+					</div>
 				</Col>
 			</Row>
 		</Modal>
