1004a67669794f536909d78ae409565af15af77a
nour-gamal <nmg181963@hotmail.com>
2022-01-17 16:38:52 +0200

    product card in market place

--- src/Modules/Suppliers/Components/ProductCard/ProductCard.js
+++ src/Modules/Suppliers/Components/ProductCard/ProductCard.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { baseUrl } from "../../../../Services";
 import "./ProductCard.css";
 function ProductCard({ product }) {
 	return (
 		<div className="productCard my-2">
-			<img src={product.image} alt="productImage" className="productImage" />
+			<img
+				src={baseUrl + product.image}
+				alt="productImage"
+				className="productImage"
+			/>
 			<div className="f-17 productName fw-600 my-2">{product.name}</div>
 			<div className="storeInfo">
 				<img
-					src={product.storeImage}
+					src={baseUrl + product.companyLogo}
 					alt="storeName"
 					className=" mx-2 rounded-circle"
 				/>
-				<div className="f-14">{product.storeName}</div>
+				<div className="f-14">{product.companyName}</div>
 			</div>
 		</div>
 	);
