422c76639f30523b2ac8b65572ad6c0a6a9315b9
a. Elkhalily <a.elkhalily10@gmail.com>
2022-02-08 14:37:24 +0200

    hands on Notifications && rfqInvitation fix height

--- src/Modules/Common/Navbar/Navbar.js
+++ src/Modules/Common/Navbar/Navbar.js
@@ -1,129 +1,372 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
-import ShayyekLogoDarkEn from "../../../Resources/Assets/shayyekLogoDark.png";
-import ShayyekLogoLightEn from "../../../Resources/Assets/shayyekLogoLight.png";
-import ShayyekLogoDarkAr from "../../../Resources/Assets/ShayyekLogoDarkAr.png";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import languages from "../../../Resources/Assets/languages.svg";
+import { Navbar } from "react-bootstrap";
+import { Scrollbars } from "react-custom-scrollbars-2";
+import { Dropdown, Button, List, Avatar } from "antd";
 import GuestNav from "./GuestNav";
+import UserNav from "./UserNav";
 import { changeLocal } from "../../../Redux/Localization";
+import ShayyekLogoDarkAr from "../../../Resources/Assets/ShayyekLogoDarkAr.png";
+import ShayyekLogoDarkEn from "../../../Resources/Assets/shayyekLogoDark.png";
+import ShayyekLogoLightEn from "../../../Resources/Assets/shayyekLogoLight.png";
 import Chat from "../../../Resources/Assets/ChatIcon.png";
 import Notification from "../../../Resources/Assets/Notification Icon.svg";
-import UserNav from "./UserNav";
+import languages from "../../../Resources/Assets/languages.svg";
 import cart from "../../../Resources/Assets/cart.svg";
 import AllSuppliers from "../../../Resources/Assets/All_suppliers.svg";
+import userAvatar from "../../../Resources/Assets/people.svg";
 import { authorType } from "../../../helpers/authType";
 import "./Navbar.css";
 
 function Navbarr({ navState, verifayState, transparent }) {
-	const dispatch = useDispatch();
-	const { currentLocal } = useSelector((state) => state.currentLocal);
-	const { currentLanguageId } = useSelector((state) => state.currentLocal);
-	const { authorization } = useSelector((state) => state.authorization);
-	const loginState = authorization.userTypeId ? true : false;
-	const {
-		authorization: { userTypeId, accountTypeId, roleId },
-	} = useSelector((state) => state.authorization);
-	const isBuyer =
-		authorType(accountTypeId, userTypeId, roleId) &&
-		authorType(accountTypeId, userTypeId, roleId).includes("buyer");
+  const dispatch = useDispatch();
+  const { currentLocal } = useSelector((state) => state.currentLocal);
+  const { currentLanguageId } = useSelector((state) => state.currentLocal);
+  const { authorization } = useSelector((state) => state.authorization);
+  const loginState = authorization.userTypeId ? true : false;
+  const {
+    authorization: { userTypeId, accountTypeId, roleId },
+  } = useSelector((state) => state.authorization);
+  const isBuyer =
+    authorType(accountTypeId, userTypeId, roleId) &&
+    authorType(accountTypeId, userTypeId, roleId).includes("buyer");
 
-	return (
-		<Navbar
-			expand="lg"
-			className={
-				transparent
-					? "transparent f-14 ppl ppr"
-					: navState
-					? verifayState
-						? "light f-14 ppl ppr d-flex justify-content-between"
-						: "light f-14 ppl ppr"
-					: "dark f-14 ppl ppr"
-			}
-			variant={"dark"}
-			collapseOnSelect={true}
-		>
-			<Link to="/">
-				{navState ? (
-					currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057" ? (
-						<img src={ShayyekLogoDarkEn} alt="ShayyekLogoDark" />
-					) : (
-						<img
-							src={ShayyekLogoDarkAr}
-							style={{ height: 40 }}
-							alt="ShayyekLogoDark"
-						/>
-					)
-				) : currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057" ? (
-					<img src={ShayyekLogoLightEn} alt="ShayyekLogoLight" />
-				) : (
-					<img src={ShayyekLogoDarkAr} alt="ShayyekLogoLight" />
-				)}
-			</Link>
+  const data = [
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+    {
+      picture: {
+        large: UserNav,
+      },
+      name: {
+        last: "Mahmoud",
+      },
+      email: "a.elkahlily10@gmail.com",
+    },
+  ];
+  const notificationMenu = (
+    <div
+      id="scrollableDiv"
+      style={{
+        width: 400,
+        border: "1px solid rgba(140, 140, 140, 0.35)",
+      }}
+    >
+      <Scrollbars style={{ height: 400 }}>
+        <List
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item key={item.id}>
+              <List.Item.Meta
+                avatar={<Avatar src={userAvatar} />}
+                title={<a href="https://ant.design">{item.name.last}</a>}
+                description={item.email}
+              />
+            </List.Item>
+          )}
+        />
+      </Scrollbars>
+    </div>
+  );
 
-			{!navState && loginState && (
-				<span className="controlIcon d-flex justify-content-end">
-					{isBuyer && (
-						<span className="align-content-center d-flex">
-							<Link to="/suppliers" className="nav-link d-none d-lg-inline">
-								<img src={AllSuppliers} alt="AllSuppliers" />
-								<span className="color-white mx-1 ">
-									{currentLocal.navbar.AllSuppliers}
-								</span>
-							</Link>
-							<Link to="/cart" className="nav-link d-none d-lg-inline">
-								<img src={cart} alt="cart" />
-								<span className="color-white mx-1 ">
-									{currentLocal.navbar.cart}
-								</span>
-							</Link>
-						</span>
-					)}
-					<Link to="/chat" className="nav-link">
-						<img src={Chat} alt="Chat" />
-					</Link>
-					<Link to="/registration" className="nav-link  registration">
-						<img src={Notification} alt="Notification" />
-					</Link>
-				</span>
-			)}
+  return (
+    <Navbar
+      expand="lg"
+      className={
+        transparent
+          ? "transparent f-14 ppl ppr"
+          : navState
+          ? verifayState
+            ? "light f-14 ppl ppr d-flex justify-content-between"
+            : "light f-14 ppl ppr"
+          : "dark f-14 ppl ppr"
+      }
+      variant={"dark"}
+      collapseOnSelect={true}
+    >
+      <Link to="/">
+        {navState ? (
+          currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057" ? (
+            <img src={ShayyekLogoDarkEn} alt="ShayyekLogoDark" />
+          ) : (
+            <img
+              src={ShayyekLogoDarkAr}
+              style={{ height: 40 }}
+              alt="ShayyekLogoDark"
+            />
+          )
+        ) : currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057" ? (
+          <img src={ShayyekLogoLightEn} alt="ShayyekLogoLight" />
+        ) : (
+          <img src={ShayyekLogoDarkAr} alt="ShayyekLogoLight" />
+        )}
+      </Link>
 
-			{verifayState && (
-				<>
-					<div className="lang">
-						<span className="languageWord mx-2">
-							{currentLocal.language === "العربيه" ? "عربي" : "English"}
-						</span>
-						<span>
-							<img
-								src={languages}
-								alt="languages"
-								onClick={() => {
-									dispatch(
-										changeLocal(
-											currentLocal.language === "English" ? "ar" : "en"
-										)
-									);
-								}}
-								className="languages"
-							/>
-						</span>
-					</div>
-				</>
-			)}
-			{!navState && <Navbar.Toggle aria-controls="basic-navbar-nav" />}
+      {!navState && loginState && (
+        <span className="controlIcon d-flex justify-content-end">
+          {isBuyer && (
+            <span className="align-content-center d-flex">
+              <Link to="/suppliers" className="nav-link d-none d-lg-inline">
+                <img src={AllSuppliers} alt="AllSuppliers" />
+                <span className="color-white mx-1 ">
+                  {currentLocal.navbar.AllSuppliers}
+                </span>
+              </Link>
+              <Link to="/cart" className="nav-link d-none d-lg-inline">
+                <img src={cart} alt="cart" />
+                <span className="color-white mx-1 ">
+                  {currentLocal.navbar.cart}
+                </span>
+              </Link>
+            </span>
+          )}
+          <Link to="/chat" className="nav-link">
+            <img src={Chat} alt="Chat" />
+          </Link>
+          <Dropdown overlay={notificationMenu} trigger={["click"]}>
+            <Button
+              type="text"
+              className="nav-link"
+              style={{ padding: "0.5rem 1rem" }}
+            >
+              <img src={Notification} alt="Notification" />
+            </Button>
+          </Dropdown>
+        </span>
+      )}
 
-			{!navState && (
-				<Navbar.Collapse
-					id="basic-navbar-nav"
-					className={loginState ? "flex-grow-0" : "flex-grow-1"}
-				>
-					{!loginState ? <GuestNav /> : <UserNav loginState={loginState} />}
-				</Navbar.Collapse>
-			)}
-		</Navbar>
-	);
+      {verifayState && (
+        <>
+          <div className="lang">
+            <span className="languageWord mx-2">
+              {currentLocal.language === "العربيه" ? "عربي" : "English"}
+            </span>
+            <span>
+              <img
+                src={languages}
+                alt="languages"
+                onClick={() => {
+                  dispatch(
+                    changeLocal(
+                      currentLocal.language === "English" ? "ar" : "en"
+                    )
+                  );
+                }}
+                className="languages"
+              />
+            </span>
+          </div>
+        </>
+      )}
+      {!navState && <Navbar.Toggle aria-controls="basic-navbar-nav" />}
+
+      {!navState && (
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className={loginState ? "flex-grow-0" : "flex-grow-1"}
+        >
+          {!loginState ? <GuestNav /> : <UserNav loginState={loginState} />}
+        </Navbar.Collapse>
+      )}
+    </Navbar>
+  );
 }
 
 export default Navbarr;
