cbc3a91dd59339c6e59c1fc8a0985a2c6cf3e3d0
nour-gamal <nmg181963@hotmail.com>
2022-03-10 10:56:03 +0200

    clean up

--- src/Modules/Messages/CreateOnlineSessionModal/CreateOnlineSessionModal.js
+++ src/Modules/Messages/CreateOnlineSessionModal/CreateOnlineSessionModal.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { doc, setDoc, collection, addDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { minBy } from "lodash";
 import { Modal, Checkbox, Row, Col, DatePicker } from "antd";
 import {
-  GetSuppliersAndContratorsThatFilledRFQ,
-  AddOnlineSession,
+	GetSuppliersAndContratorsThatFilledRFQ,
+	AddOnlineSession,
 } from "../network";
 import { db } from "../../../firebase";
 import { baseUrl } from "../../../Services";
@@ -14,147 +14,147 @@ import defaultProfileImage from "../../../Resources/Assets/DefaultProfileImage.p
 import "./CreateOnlineSessionModal.css";
 
 const CreateOnineSession = ({ isModalVisible, onCancel, rfqId }) => {
-  const { currentLocal } = useSelector((state) => state.currentLocal);
-  const { authorization } = useSelector((state) => state.authorization);
-  const [members, setMembers] = useState(null);
-  const [time, updateTime] = useState(null);
-  const [selectedMembers, updateSelectedMembers] = useState([]);
-  const [selectedMemberIds, updateSelectedMemberIds] = useState([]);
-  const [disableSubmit, updateDisableSubmit] = useState(true);
+	const { currentLocal } = useSelector((state) => state.currentLocal);
+	const { authorization } = useSelector((state) => state.authorization);
+	const [members, setMembers] = useState(null);
+	const [time, updateTime] = useState(null);
+	const [selectedMembers, updateSelectedMembers] = useState([]);
+	const [selectedMemberIds, updateSelectedMemberIds] = useState([]);
+	const [disableSubmit, updateDisableSubmit] = useState(true);
 
-  useEffect(() => {
-    GetSuppliersAndContratorsThatFilledRFQ(
-      rfqId,
-      (success) => {
-        if (success.success) {
-          setMembers(success.data);
-        }
-      },
-      (fail) => {}
-    );
-  }, [rfqId]);
+	useEffect(() => {
+		GetSuppliersAndContratorsThatFilledRFQ(
+			rfqId,
+			(success) => {
+				if (success.success) {
+					setMembers(success.data);
+				}
+			},
+			(fail) => {}
+		);
+	}, [rfqId]);
 
-  // add new online-session
-  function submitOnlineSession() {
-    console.log(selectedMembers);
-    if (selectedMembers.length && time) {
-      // users for firebase
-      let users = [];
-      selectedMembers.forEach(async (userData) => {
-        const { userId } = userData;
-        let userDoc = doc(db, `users/${userId}`);
-        users.push({
-          user: userDoc,
-          isOnline: false,
-          lastPrice: 0,
-        });
-      });
+	// add new online-session
+	function submitOnlineSession() {
+		console.log(selectedMembers);
+		if (selectedMembers.length && time) {
+			// users for firebase
+			let users = [];
+			selectedMembers.forEach(async (userData) => {
+				const { userId } = userData;
+				let userDoc = doc(db, `users/${userId}`);
+				users.push({
+					user: userDoc,
+					isOnline: false,
+					lastPrice: 0,
+				});
+			});
 
-      let data = {
-        rfqId,
-        membersIds: selectedMemberIds,
-        sessionDate: time._d,
-        projectName: "static project name!",
-      };
-      AddOnlineSession(
-        data,
-        async (success) => {
-          if (success.success) {
-            const {
-              data: { sessionId },
-            } = success;
+			let data = {
+				rfqId,
+				membersIds: selectedMemberIds,
+				sessionDate: time._d,
+				projectName: "static project name!",
+			};
+			AddOnlineSession(
+				data,
+				async (success) => {
+					if (success.success) {
+						const {
+							data: { sessionId },
+						} = success;
 
-            const { price } = minBy(selectedMembers, "price");
-            await setDoc(doc(db, "online-sessions", sessionId), {
-              rfqId,
-              users,
-              sessionDate: time._d,
-              buyerId: authorization.id,
-              lastPrice: price,
-            });
-            onCancel();
-          }
-        },
-        (fail) => {}
-      );
-    }
-  }
+						const { price } = minBy(selectedMembers, "price");
+						await setDoc(doc(db, "online-sessions", sessionId), {
+							rfqId,
+							users,
+							sessionDate: time._d,
+							buyerId: authorization.id,
+							lastPrice: price,
+						});
+						onCancel();
+					}
+				},
+				(fail) => {}
+			);
+		}
+	}
 
-  // update submitbutton disabled property
-  useEffect(() => {
-    if (selectedMembers.length && time) updateDisableSubmit(false);
-    else updateDisableSubmit(true);
-  }, [time, selectedMembers]);
+	// update submitbutton disabled property
+	useEffect(() => {
+		if (selectedMembers.length && time) updateDisableSubmit(false);
+		else updateDisableSubmit(true);
+	}, [time, selectedMembers]);
 
-  //set users on the online session
-  function setMembersOnSelectCheckbox(data) {
-    let memberIds = [];
-    data.forEach((item) => {
-      const { userId } = item;
-      memberIds.push(userId);
-    });
-    updateSelectedMembers(data);
-    updateSelectedMemberIds(memberIds);
-  }
+	//set users on the online session
+	function setMembersOnSelectCheckbox(data) {
+		let memberIds = [];
+		data.forEach((item) => {
+			const { userId } = item;
+			memberIds.push(userId);
+		});
+		updateSelectedMembers(data);
+		updateSelectedMemberIds(memberIds);
+	}
 
-  return (
-    <Modal
-      visible={isModalVisible}
-      onCancel={() => onCancel()}
-      className="createOnlineSession-modal modal-lg"
-    >
-      <div className="createOnlineSession-body">
-        <Row>
-          <Col md={12}>
-            <header>
-              <h4>{currentLocal.messages.selectOnlineSessionMembers}</h4>
-            </header>
-            <form>
-              <ul className="createOnlineSession-items">
-                <Checkbox.Group onChange={setMembersOnSelectCheckbox}>
-                  {members &&
-                    members.map((member) => (
-                      <li className="item d-flex" key={member.userId}>
-                        <Checkbox value={member} />
-                        <img
-                          src={
-                            member.userImage
-                              ? baseUrl + member.userImage
-                              : defaultProfileImage
-                          }
-                          alt="user-avatar"
-                        />
-                        <p>{member.userName}</p>
-                      </li>
-                    ))}
-                </Checkbox.Group>
-              </ul>
-            </form>
-          </Col>
-          <Col md={12}>
-            <header>
-              <h4>{currentLocal.messages.pickOnlineSession}</h4>
-            </header>
-            <DatePicker
-              showTime
-              onOk={(data) => updateTime(data)}
-              onChange={(data) => updateTime(data)}
-            />
-          </Col>
-        </Row>
-      </div>
-      <div className="createOnlineSession-footer text-center">
-        <button
-          disabled={disableSubmit}
-          className="btn button-primary"
-          type="submit"
-          onClick={submitOnlineSession}
-        >
-          Submit
-        </button>
-      </div>
-    </Modal>
-  );
+	return (
+		<Modal
+			visible={isModalVisible}
+			onCancel={() => onCancel()}
+			className="createOnlineSession-modal modal-lg"
+		>
+			<div className="createOnlineSession-body">
+				<Row>
+					<Col md={12}>
+						<header>
+							<h4>{currentLocal.messages.selectOnlineSessionMembers}</h4>
+						</header>
+						<form>
+							<ul className="createOnlineSession-items">
+								<Checkbox.Group onChange={setMembersOnSelectCheckbox}>
+									{members &&
+										members.map((member) => (
+											<li className="item d-flex" key={member.userId}>
+												<Checkbox value={member} />
+												<img
+													src={
+														member.userImage
+															? baseUrl + member.userImage
+															: defaultProfileImage
+													}
+													alt="user-avatar"
+												/>
+												<p>{member.userName}</p>
+											</li>
+										))}
+								</Checkbox.Group>
+							</ul>
+						</form>
+					</Col>
+					<Col md={12}>
+						<header>
+							<h4>{currentLocal.messages.pickOnlineSession}</h4>
+						</header>
+						<DatePicker
+							showTime
+							onOk={(data) => updateTime(data)}
+							onChange={(data) => updateTime(data)}
+						/>
+					</Col>
+				</Row>
+			</div>
+			<div className="createOnlineSession-footer text-center">
+				<button
+					disabled={disableSubmit}
+					className="btn button-primary"
+					type="submit"
+					onClick={submitOnlineSession}
+				>
+					Submit
+				</button>
+			</div>
+		</Modal>
+	);
 };
 
 export default CreateOnineSession;
--- src/Modules/Messages/StartOnlineSession/StartOnlineSession.js
+++ src/Modules/Messages/StartOnlineSession/StartOnlineSession.js
@@ -10,129 +10,130 @@ import user from "../../../Resources/Assets/MessageAvatar.png";
 import "./StartOnlineSession.css";
 
 const StartOnlineSession = ({ isModalVisible, onCancel }) => {
-  const { authorization } = useSelector((state) => state.authorization);
-  const { currentLocal } = useSelector((state) => state.currentLocal);
-  const [offerPrice, setOfferPrice] = useState("");
-  const [messages, updateMessages] = useState([]);
-  const scrollTo = useRef();
-  async function sendOffer(e) {
-    e.preventDefault();
+	const { authorization } = useSelector((state) => state.authorization);
+	const { currentLocal } = useSelector((state) => state.currentLocal);
+	const [offerPrice, setOfferPrice] = useState("");
+	// eslint-disable-next-line
+	const [messages, updateMessages] = useState([]);
+	const scrollTo = useRef();
+	async function sendOffer(e) {
+		e.preventDefault();
 
-    let userDocRef = doc(db, `users/${authorization.id}`);
-    const data = {
-      sender: userDocRef,
-      content: offerPrice,
-      badget: false,
-    };
-    await addDoc(
-      collection(
-        db,
-        "online-sessions",
-        "7d6962a2-3db3-41e6-b1ef-432741a4bf0f",
-        "messages"
-      ),
-      data
-    );
-    // data.messageId += 1;
-    // updateMessages((prevMessages) => [...prevMessages, data]);
-    setOfferPrice("");
-    scrollTo.current.scrollIntoView({ behavior: "smooth" });
-  }
+		let userDocRef = doc(db, `users/${authorization.id}`);
+		const data = {
+			sender: userDocRef,
+			content: offerPrice,
+			badget: false,
+		};
+		await addDoc(
+			collection(
+				db,
+				"online-sessions",
+				"7d6962a2-3db3-41e6-b1ef-432741a4bf0f",
+				"messages"
+			),
+			data
+		);
+		// data.messageId += 1;
+		// updateMessages((prevMessages) => [...prevMessages, data]);
+		setOfferPrice("");
+		scrollTo.current.scrollIntoView({ behavior: "smooth" });
+	}
 
-  useEffect(() => {
-    async function getMessages() {
-      const docRef = doc(
-        db,
-        "online-sessions",
-        "7d6962a2-3db3-41e6-b1ef-432741a4bf0f"
-      );
+	useEffect(() => {
+		async function getMessages() {
+			const docRef = doc(
+				db,
+				"online-sessions",
+				"7d6962a2-3db3-41e6-b1ef-432741a4bf0f"
+			);
 
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        const { users } = docSnap.data();
-        users?.forEach(async (item) => {
-          let { user } = await item;
-          let docSnapshot = await getDoc(user);
-          console.log(docSnapshot.data());
-        });
-      } else {
-        console.log("there's no messages here");
-      }
-    }
-    getMessages();
-  }, []);
-  return (
-    <Modal
-      visible={isModalVisible}
-      onCancel={() => onCancel()}
-      className="StartOnlineSession-modal modal-lg"
-    >
-      <header className="d-flex flex-wrap">
-        <div className="user justify-content-start">
-          <img className="userImage" src={user} alt="user-avatar" />
-          <p>Project Name</p>
-        </div>
-        <div className="lastPrice">
-          {currentLocal.messages.lastPrice} 1200 LE
-        </div>
-        <div className="suppliers d-flex">
-          <p className=" me-2">{currentLocal.messages.suppliers}</p>
-          <img className="userImage" src={user} alt="user-avatar" />
-          <img className="userImage" src={user} alt="user-avatar" />
-          <img className="userImage" src={user} alt="user-avatar" />
-          <img className="userImage" src={user} alt="user-avatar" />
-        </div>
-      </header>
-      <div className="message-content mt-2 flex-1">
-        <ul>
-          {messages?.map((message) => (
-            <div className="content" key={message.messageId}>
-              <img
-                className="message-content__user"
-                src={message.senderImage}
-                alt="user-avatar"
-              />
-              <div>
-                <h6>{message.senderName}</h6>
-                <p>{message.senderPrice}</p>
-              </div>
-            </div>
-          ))}
-          <div ref={scrollTo}></div>
-        </ul>
-      </div>
-      <div className="StartOnlineSession__sendMessages mt-2 d-flex justify-content-between align-items-center">
-        <div className="d-flex">
-          <Button type="text">
-            <img src={EndSession} alt="end-session" />
-          </Button>
-          <Button type="text">
-            <img src={BadgeSession} alt="badge-session" />
-          </Button>
-        </div>
-        <form onSubmit={sendOffer} className="flex-1 d-flex align-items-center">
-          <input
-            type="number"
-            className="messageInput"
-            value={offerPrice}
-            placeholder={currentLocal.messages.enterOfferPrice}
-            onChange={(e) => setOfferPrice(e.target.value)}
-          />
-          <div className="d-flex icons-inside-form-field">
-            <Button type="text">
-              <img src={EndSession} alt="end-session" />
-            </Button>
-            <Button type="text">
-              <img src={BadgeSession} alt="badge-session" />
-            </Button>
-          </div>
-          <Button type="text" htmlType="submit">
-            <img className="flip-image" src={SendSession} alt="send-session" />
-          </Button>
-        </form>
-      </div>
-    </Modal>
-  );
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				const { users } = docSnap.data();
+				users?.forEach(async (item) => {
+					let { user } = await item;
+					let docSnapshot = await getDoc(user);
+					console.log(docSnapshot.data());
+				});
+			} else {
+				console.log("there's no messages here");
+			}
+		}
+		getMessages();
+	}, []);
+	return (
+		<Modal
+			visible={isModalVisible}
+			onCancel={() => onCancel()}
+			className="StartOnlineSession-modal modal-lg"
+		>
+			<header className="d-flex flex-wrap">
+				<div className="user justify-content-start">
+					<img className="userImage" src={user} alt="user-avatar" />
+					<p>Project Name</p>
+				</div>
+				<div className="lastPrice">
+					{currentLocal.messages.lastPrice} 1200 LE
+				</div>
+				<div className="suppliers d-flex">
+					<p className=" me-2">{currentLocal.messages.suppliers}</p>
+					<img className="userImage" src={user} alt="user-avatar" />
+					<img className="userImage" src={user} alt="user-avatar" />
+					<img className="userImage" src={user} alt="user-avatar" />
+					<img className="userImage" src={user} alt="user-avatar" />
+				</div>
+			</header>
+			<div className="message-content mt-2 flex-1">
+				<ul>
+					{messages?.map((message) => (
+						<div className="content" key={message.messageId}>
+							<img
+								className="message-content__user"
+								src={message.senderImage}
+								alt="user-avatar"
+							/>
+							<div>
+								<h6>{message.senderName}</h6>
+								<p>{message.senderPrice}</p>
+							</div>
+						</div>
+					))}
+					<div ref={scrollTo}></div>
+				</ul>
+			</div>
+			<div className="StartOnlineSession__sendMessages mt-2 d-flex justify-content-between align-items-center">
+				<div className="d-flex">
+					<Button type="text">
+						<img src={EndSession} alt="end-session" />
+					</Button>
+					<Button type="text">
+						<img src={BadgeSession} alt="badge-session" />
+					</Button>
+				</div>
+				<form onSubmit={sendOffer} className="flex-1 d-flex align-items-center">
+					<input
+						type="number"
+						className="messageInput"
+						value={offerPrice}
+						placeholder={currentLocal.messages.enterOfferPrice}
+						onChange={(e) => setOfferPrice(e.target.value)}
+					/>
+					<div className="d-flex icons-inside-form-field">
+						<Button type="text">
+							<img src={EndSession} alt="end-session" />
+						</Button>
+						<Button type="text">
+							<img src={BadgeSession} alt="badge-session" />
+						</Button>
+					</div>
+					<Button type="text" htmlType="submit">
+						<img className="flip-image" src={SendSession} alt="send-session" />
+					</Button>
+				</form>
+			</div>
+		</Modal>
+	);
 };
 
 export default StartOnlineSession;
