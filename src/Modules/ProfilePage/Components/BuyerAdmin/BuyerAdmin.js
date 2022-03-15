a07d2247c87da50fbfce27451311ee74b4027f32
nour-gamal <nmg181963@hotmail.com>
2022-03-15 17:54:53 +0200

    clean up

--- src/Modules/ProfilePage/Components/BuyerAdmin/BuyerAdmin.js
+++ src/Modules/ProfilePage/Components/BuyerAdmin/BuyerAdmin.js
@@ -10,82 +10,73 @@ import SidePersonalInfo from "../SubComponents/SidePersonalInfo/SidePersonalInfo
 import { getBuyerProfile } from "../../network";
 import "./BuyerAdmin.css";
 function BuyerAdmin() {
-  const { currentLocal } = useSelector((state) => state.currentLocal);
-  const { currentLanguageId } = useSelector((state) => state.currentLocal);
-  const [buyerChartWorks, updateBuyerChartWorks] = useState([]);
-  const [buyerRFQs, updataBuyerRFQs] = useState([]);
-  const [buyerOrders, updateBuyerOrders] = useState([]);
-  const [company, updateCompany] = useState(null);
-  const [allData, updateAllDate] = useState({});
-  // const labelData = [
-  // 	{
-  // 		label:
-  // 			currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057"
-  // 				? "Consultant"
-  // 				: "مستشار",
-  // 		color: "#00172A",
-  // 	},
-  // ];
+	const { currentLocal } = useSelector((state) => state.currentLocal);
+	const { currentLanguageId } = useSelector((state) => state.currentLocal);
+	const [buyerChartWorks, updateBuyerChartWorks] = useState([]);
+	const [buyerRFQs, updataBuyerRFQs] = useState([]);
+	const [buyerOrders, updateBuyerOrders] = useState([]);
+	const [company, updateCompany] = useState(null);
+	const [allData, updateAllDate] = useState({});
+	// const labelData = [
+	// 	{
+	// 		label:
+	// 			currentLanguageId === "274c0b77-90cf-4ee3-976e-01e409413057"
+	// 				? "Consultant"
+	// 				: "مستشار",
+	// 		color: "#00172A",
+	// 	},
+	// ];
 
-  useEffect(() => {
-    getBuyerProfile(
-      currentLanguageId,
-      (success) => {
-        updateBuyerChartWorks(success.data.buyerChartWorks);
-        updataBuyerRFQs(success.data.buyerRFQs);
-        updateBuyerOrders(success.data.buyerOrders);
-        updateCompany(success.data.company);
-        updateAllDate(success.data);
-      },
-      (fail) => {
-        console.log(fail);
-      }
-    );
-  }, [currentLanguageId]);
-  return (
-    <div className={currentLocal.language === "English" ? "ppl" : "ppr"}>
-      <Row>
-        <Col md={19} xs={16} className="pr-1">
-          <PersonalInfo parent={"buyerAdmin"} company={company} />
-          <Row>
-            {buyerChartWorks.map((data, indexData) => (
-              <Col
-                key={indexData}
-                md={12}
-                lg={8}
-                xl={6}
-                className="margin-auto"
-              >
-                <Quarter chartData={data} />
-              </Col>
-            ))}
-          </Row>
-
-          {/* <div className="d-flex">
-						{labelData.map((label) => (
-							<div className="colorLabel d-flex">
-								<div style={{ background: label }}></div>
-								<div>test</div>
-							</div>
+	useEffect(() => {
+		getBuyerProfile(
+			currentLanguageId,
+			(success) => {
+				updateBuyerChartWorks(success.data.buyerChartWorks);
+				updataBuyerRFQs(success.data.buyerRFQs);
+				updateBuyerOrders(success.data.buyerOrders);
+				updateCompany(success.data.company);
+				updateAllDate(success.data);
+			},
+			(fail) => {
+				console.log(fail);
+			}
+		);
+	}, [currentLanguageId]);
+	return (
+		<div className={currentLocal.language === "English" ? "ppl" : "ppr"}>
+			<Row>
+				<Col md={19} xs={16} className="pr-1">
+					<PersonalInfo parent={"buyerAdmin"} company={company} />
+					<Row>
+						{buyerChartWorks.map((data, indexData) => (
+							<Col
+								key={indexData}
+								md={12}
+								lg={8}
+								xl={6}
+								className="margin-auto"
+							>
+								<Quarter chartData={data} />
+							</Col>
 						))}
-					</div> */}
+					</Row>
 
-          <MyRFQs buyerRFQs={buyerRFQs} />
-          <MyOrders />
-        </Col>
-        <Col md={5} xs={8}>
-          <div className="profileSideMenu">
-            <SidePersonalInfo allData={allData} />
-            <CompanyCard
-              buyerOrders={buyerOrders}
-              companyDetails={company}
-              sidebar={true}
-            />
-          </div>
-        </Col>
-      </Row>
-    </div>
-  );
+					<MyRFQs buyerRFQs={buyerRFQs} />
+					<MyOrders />
+				</Col>
+				<Col md={5} xs={8}>
+					<div className="profileSideMenu">
+						<SidePersonalInfo allData={allData} />
+						<CompanyCard
+							buyerOrders={buyerOrders}
+							companyDetails={company}
+							sidebar={true}
+						/>
+					</div>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
 export default BuyerAdmin;
