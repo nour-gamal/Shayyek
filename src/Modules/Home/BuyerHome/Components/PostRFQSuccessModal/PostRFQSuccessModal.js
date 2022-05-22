b601aacc5530ba60fcf93a574f5923a9016b2d52
ahmed-elkhalily <a.elkhalily10@gmail.com>
2022-05-22 02:17:22 -0700

    cleanup

--- src/Modules/Home/BuyerHome/Components/PostRFQSuccessModal/PostRFQSuccessModal.js
+++ src/Modules/Home/BuyerHome/Components/PostRFQSuccessModal/PostRFQSuccessModal.js
@@ -9,88 +9,89 @@ import { postRFQ } from "../../../network";
 import "./PostRFQSuccessModal.css";
 
 function PostRFQSuccessModal({ isModalVisible, onCancel, alreadyHasPackage }) {
-	const { currentLocal } = useSelector((state) => state.currentLocal);
-	const { rfqData } = useSelector((state) => state.rfq);
-	const [redirectTo, updateRedirectTo] = useState(null);
-	const dispatch = useDispatch();
-	const handleSubmit = () => {
-		let data = {
-			...rfqData,
-		};
-		postRFQ(
-			data,
-			(success) => {
-				if (success.data) {
-					if (rfqData.inviteByWhatsapp) {
-						window.open(
-							`https://api.whatsapp.com/send?text=${success.data.data}`,
-							"_blank"
-						);
-					}
-					clearRFQStore();
-					updateRedirectTo("/");
-				}
-			},
-			(fail) => {
-				console.log(fail);
-			}
-		);
-	};
-	const clearRFQStore = () => {
-		dispatch(addRFQDetails({}));
-	};
-	if (redirectTo) return <Redirect to={redirectTo} />;
-	return (
-		<Modal
-			title="Basic Modal"
-			visible={isModalVisible}
-			onCancel={onCancel}
-			className="modal-sm postRFQSuccessModal"
-		>
-			<img src={packageFile} alt="errorFile" />
-			<div className="text-center my-4">
-				<div className="my-4">
-					{!alreadyHasPackage ? (
-						<div>{currentLocal.buyerHome.readyToSubmit}</div>
-					) : (
-						<div>
-							<div>{currentLocal.buyerHome.beforeSumbit}</div>
-							<div>{currentLocal.buyerHome.addAnotherPackage}</div>
-						</div>
-					)}
-				</div>
-				{alreadyHasPackage ? (
-					<div>
-						<button
-							className="button-secondary flat m-2"
-							onClick={() => {
-								window.location.reload();
-							}}
-						>
-							{currentLocal.buyerHome.addPackage}
-						</button>
-						<button className="button-primary flat m-2" onClick={handleSubmit}>
-							{currentLocal.buyerHome.submit}
-						</button>
-					</div>
-				) : (
-					<div>
-						<button
-							className="button-secondary flat m-2"
-							onClick={() => {
-								onCancel();
-							}}
-						>
-							{currentLocal.buyerHome.cancel}
-						</button>
-						<button className="button-primary flat m-2" onClick={handleSubmit}>
-							{currentLocal.buyerHome.submit}
-						</button>
-					</div>
-				)}
-			</div>
-		</Modal>
-	);
+  const { currentLocal } = useSelector((state) => state.currentLocal);
+  const { rfqData } = useSelector((state) => state.rfq);
+  const [redirectTo, updateRedirectTo] = useState(null);
+  const dispatch = useDispatch();
+  const handleSubmit = () => {
+    let data = {
+      ...rfqData,
+    };
+    postRFQ(
+      data,
+      (success) => {
+        if (success.data) {
+          if (rfqData.inviteByWhatsapp) {
+            window.open(
+              `https://api.whatsapp.com/send?text=${success.data.data}`,
+              "_blank"
+            );
+          }
+          clearRFQStore();
+          updateRedirectTo("/");
+        }
+      },
+      (fail) => {
+        console.log(fail);
+      }
+    );
+  };
+  const clearRFQStore = () => {
+    dispatch(addRFQDetails({}));
+  };
+
+  if (redirectTo) return <Redirect to={redirectTo} />;
+  return (
+    <Modal
+      title="Basic Modal"
+      visible={isModalVisible}
+      onCancel={onCancel}
+      className="modal-sm postRFQSuccessModal"
+    >
+      <img src={packageFile} alt="errorFile" />
+      <div className="text-center my-4">
+        <div className="my-4">
+          {!alreadyHasPackage ? (
+            <div>{currentLocal.buyerHome.readyToSubmit}</div>
+          ) : (
+            <div>
+              <div>{currentLocal.buyerHome.beforeSumbit}</div>
+              <div>{currentLocal.buyerHome.addAnotherPackage}</div>
+            </div>
+          )}
+        </div>
+        {alreadyHasPackage ? (
+          <div>
+            <button
+              className="button-secondary flat m-2"
+              onClick={() => {
+                window.location.reload();
+              }}
+            >
+              {currentLocal.buyerHome.addPackage}
+            </button>
+            <button className="button-primary flat m-2" onClick={handleSubmit}>
+              {currentLocal.buyerHome.submit}
+            </button>
+          </div>
+        ) : (
+          <div>
+            <button
+              className="button-secondary flat m-2"
+              onClick={() => {
+                onCancel();
+              }}
+            >
+              {currentLocal.buyerHome.cancel}
+            </button>
+            <button className="button-primary flat m-2" onClick={handleSubmit}>
+              {currentLocal.buyerHome.submit}
+            </button>
+          </div>
+        )}
+      </div>
+    </Modal>
+  );
 }
 
 export default PostRFQSuccessModal;
